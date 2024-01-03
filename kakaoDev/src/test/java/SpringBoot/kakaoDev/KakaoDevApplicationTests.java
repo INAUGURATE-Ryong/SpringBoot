89e6dbb98d583d9b668759dbee1a8a4f304d9eee
INAUGURATE-Ryong <62015109+INAUGURATE-Ryong@users.noreply.github.com>
2024-01-03 15:36:29 +0900

    [add] kakao 우편검색

---
+++ kakaoDev/src/test/java/SpringBoot/kakaoDev/KakaoDevApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBoot.kakaoDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
